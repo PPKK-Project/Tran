9bb868c9caaba37e8abfce280edae95eeb736d11
nabell0 <pmy738170@gmail.com>
2025-11-03 11:34:28 +0900

    feat:embassy 테이블 추가

--- BACK/src/main/java/com/project/team/Repository/AccommodationRepository.java
+++ BACK/src/main/java/com/project/team/Repository/AccommodationRepository.java
@@ -4,4 +4,5 @@ import com.project.team.Entity.Accommodation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+
 }
