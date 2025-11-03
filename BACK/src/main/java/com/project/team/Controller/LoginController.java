3c7ef6842c2433a9535b4c40de3cdeb6cff545db
sun <sunyp0713@naver.com>
2025-11-03 10:10:52 +0900

    UserRepository add

---
+++ BACK/src/main/java/com/project/team/Controller/LoginController.java
@@ -0,0 +1,4 @@
+package com.project.team.Controller;
+
+public class LoginController {
+}
---
+++ BACK/src/main/java/com/project/team/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.team.Repository;
+
+import com.project.team.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
