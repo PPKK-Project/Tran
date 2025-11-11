a81100d4630d866092d90fae25192d20a7ded086
sun <sunyp0713@naver.com>
2025-11-11 12:05:59 +0900

    login

--- FRONT/teamproject/src/components/login/SignIn.tsx
+++ FRONT/teamproject/src/components/login/SignIn.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import { Dialog, DialogContent, DialogTitle, TextField, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import axios, { AxiosRequestConfig } from "axios";
+
+type User = {
+  email: string;
+  password: string;
+}
 
 function SignIn() {
+  const [ isAuthenticated, setAuth ] = useState(false);
   const [open, setOpen] = useState(false);
+  const [ user, setUser ] = useState<User>({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUser({...user, [event.target.name]: event.target.value});
+  }
 
   const handleOpen = () => {
     setOpen(true);
@@ -13,7 +28,40 @@ function SignIn() {
     setOpen(false);
   };
 
-  return (
+  const getAxiosConfig = (): AxiosRequestConfig => {
+    const token = sessionStorage.getItem("jwt")?.replace("Bearer ", "");
+
+    return {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    };
+  };
+
+  const handleLogin = () => {
+    axios.post(`${import.meta.env.VITE_BASE_URL}/login`, user, {
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      const jwtToken = response.headers.authorization;
+      if(jwtToken !== null) {
+        sessionStorage.setItem("jwt", jwtToken);
+        setAuth(true);
+      }
+    })
+    .catch(err => {
+      setOpen(true);
+      console.log(err);
+    })
+  }
+
+  if(isAuthenticated) {
+    return(<>로그인 성공</>)
+  } else {
+    return (
     <>
       <button className="header-login" onClick={handleOpen}>
         로그인
@@ -34,13 +82,15 @@ function SignIn() {
         </IconButton>
         </DialogTitle>
         <DialogContent>
-          <TextField variant="outlined" margin="normal" label="이메일" fullWidth sx={{ mb: 2 }} />
-          <TextField type="password" label="비밀번호" fullWidth />
+          <TextField name="email" onChange={handleChange}variant="outlined" margin="normal" label="이메일" fullWidth sx={{ mb: 2 }} />
+          <TextField name="password" onChange={handleChange}type="password" label="비밀번호" fullWidth />
         </DialogContent>
-        <button> 로그인 하기</button> <br />
+        <button onClick={handleLogin}> 로그인 하기</button> <br />
       </Dialog>
     </>
   );
+  }
+
 }
 
 export default SignIn;
--- FRONT/teamproject/src/components/login/SignUp.tsx
+++ FRONT/teamproject/src/components/login/SignUp.tsx
@@ -1,7 +1,14 @@
 import { ChangeEvent, useState } from "react";
-import { Dialog, DialogContent, DialogTitle, TextField, IconButton } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  IconButton,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { SignUpType } from "../../../types"
+import { SignUpType } from "../../../types";
+import axios from "axios";
 
 function SignUp() {
   const [open, setOpen] = useState(false);
@@ -12,21 +19,28 @@ function SignUp() {
 
   const handleClose = () => {
     setOpen(false);
+    setSignUp({ email: "", password: "", nickname: "" });
   };
 
-  const [ signUp, setSignUp ] = useState<SignUpType>({
-    email:"",
-    password:"",
-    nickname:""
+  const [signUp, setSignUp] = useState<SignUpType>({
+    email: "",
+    password: "",
+    nickname: "",
   });
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSignUp({ ...signUp, [event.target.name]: event.target.value });
   };
 
-  const handleSave = () => {
-
-  }
+  const handleSave = async () => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/signup`, signUp);
+      alert("회원가입 완료!");
+      handleClose();
+    } catch (error) {
+      console.error("회원가입 실패:", error);
+    }
+  };
 
   return (
     <>
@@ -34,24 +48,47 @@ function SignUp() {
         회원가입
       </button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle sx={{ textAlign: "center" }}>회원 가입
+        <DialogTitle sx={{ textAlign: "center" }}>
+          회원 가입
           <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
           >
             <CloseIcon />
           </IconButton>
         </DialogTitle>
         <DialogContent>
-          <TextField name="email" value={signUp.email} onChange={handleChange} variant="outlined" margin="normal" label="이메일" fullWidth sx={{ mb: 2 }} />
-          <TextField name="password" value={signUp.password} onChange={handleChange} label="비밀번호" fullWidth sx={{ mb: 2 }} />
-          <TextField name="nickname" value={signUp.nickname} onChange={handleChange} label="닉네임" fullWidth />
+          <TextField
+            name="email"
+            value={signUp.email}
+            onChange={handleChange}
+            variant="outlined"
+            margin="normal"
+            label="이메일"
+            fullWidth
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            name="password"
+            value={signUp.password}
+            onChange={handleChange}
+            label="비밀번호"
+            fullWidth
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            name="nickname"
+            value={signUp.nickname}
+            onChange={handleChange}
+            label="닉네임"
+            fullWidth
+          />
         </DialogContent>
         <button onClick={handleSave}> 회원가입 하기</button> <br />
         <br />
